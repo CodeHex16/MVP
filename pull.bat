09eed90dac1e88b366ed40bb1050dd84b3e3fe34
lucaribon <lucaribon0203@gmail.com>
2025-04-08 02:03:06 +0200

    scripts

--- pull.bat
+++ pull.bat
@@ -1,4 +1,3 @@
 cd LLM-API/ && git pull && git checkout main && cd ..
 cd Database-API/ && git pull && git checkout main && cd ..
 cd Suppl-AI/ && git pull && git checkout main && cd ..
-docker compose up -d --build
