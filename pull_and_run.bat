02783012d5f9df846c0e0c94a74ee1336ddaeb67
Luca Ribon <64131696+lucaribon@users.noreply.github.com>
2025-04-09 16:15:56 +0200

    docker network create suppl-ai-shared-network

--- pull_and_run.bat
+++ pull_and_run.bat
@@ -1,4 +1,5 @@
 cd LLM-API/ && git pull && git checkout main && cd ..
 cd Database-API/ && git pull && git checkout main && cd ..
 cd Suppl-AI/ && git pull && git checkout main && cd ..
+docker network create suppl-ai-shared-network
 docker compose up -d --build
