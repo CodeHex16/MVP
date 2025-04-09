0bdcf3acce9dabbca616d4a10132d47fc8d0dc86
Luca Ribon <64131696+lucaribon@users.noreply.github.com>
2025-04-09 15:48:19 +0200

    Update and rename pull.bat to pull_and_run.bat

--- pull_and_run.bat
+++ pull_and_run.bat
@@ -1,3 +1,4 @@
 cd LLM-API/ && git pull && git checkout main && cd ..
 cd Database-API/ && git pull && git checkout main && cd ..
 cd Suppl-AI/ && git pull && git checkout main && cd ..
+docker compose up -d --build
