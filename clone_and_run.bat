037ede1b775b966dcf770c083c63514727fbe590
lucaribon <lucaribon0203@gmail.com>
2025-04-11 15:13:38 +0200

    fix: compose e script;

---
+++ clone_and_run.bat
@@ -0,0 +1,5 @@
+git clone https://github.com/CodeHex16/Database-API.git
+git clone https://github.com/CodeHex16/LLM-API.git
+git clone https://github.com/CodeHex16/Suppl-AI.git
+docker network create suppl-ai-shared-network
+docker compose up -d --build
