75d9cc41f7e1a87f22f2db09eb573c9f30dbde6d
lucaribon <lucaribon0203@gmail.com>
2025-04-11 17:05:10 +0200

    comment;

--- clone_and_run.sh
+++ clone_and_run.sh
@@ -2,4 +2,5 @@ git clone https://github.com/CodeHex16/Database-API.git
 git clone https://github.com/CodeHex16/LLM-API.git
 git clone https://github.com/CodeHex16/Suppl-AI.git
 docker network create suppl-ai-shared-network
+# magari mettere i file env nella root tipo llm.env e una volta clonate le repo spostarli e rinominarli
 docker compose up -d --build
