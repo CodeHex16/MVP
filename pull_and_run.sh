76e409222aba9a69c7afa442da6d480d96823906
lucaribon <lucaribon0203@gmail.com>
2025-04-29 13:43:06 +0200

    fix: script pull_and_run

--- pull_and_run.sh
+++ pull_and_run.sh
@@ -1,5 +1,5 @@
-cd LLM-API/ && git pull && git checkout main && cd ..
-cd Database-API/ && git pull && git checkout main && cd ..
-cd Suppl-AI/ && git pull && git checkout main && cd ..
+cd LLM-API/ && git pull && cd ..
+cd Database-API/ && git pull && cd ..
+cd Suppl-AI/ && git pull && cd ..
 docker network create suppl-ai-shared-network
 docker compose up -d --build
