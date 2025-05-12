3a4d7481d11d54c345ea8758e344ab41d3b51b6d
lucaribon <lucaribon0203@gmail.com>
2025-05-12 23:57:51 +0200

    refactor: remove outdated pull and run scripts

--- clone_and_run.bat
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/CodeHex16/Database-API.git
-git clone https://github.com/CodeHex16/LLM-API.git
-git clone https://github.com/CodeHex16/Suppl-AI.git
-docker network create suppl-ai-shared-network
-docker compose up -d --build
--- pull_and_run.sh
+++
@@ -1,5 +0,0 @@
-cd LLM-API/ && git pull && cd ..
-cd Database-API/ && git pull && cd ..
-cd Suppl-AI/ && git pull && cd ..
-docker network create suppl-ai-shared-network
-docker compose up -d --build
